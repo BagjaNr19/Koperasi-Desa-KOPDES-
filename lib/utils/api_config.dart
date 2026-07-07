3e8154b8df03b3975e59565aec0d4c865ca6521b
Rizky_Bagja_N <2306075@itg.ac.id>
2026-07-07 10:26:45 +0700

    update: perubahan api config terbaru

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -4,7 +4,7 @@ class ApiConfig {
   // Gunakan 10.0.2.2 untuk Android Emulator, dan 127.0.0.1 untuk Windows/Desktop
   static String get baseUrl {
     if (Platform.isAndroid) {
-      return 'http://10.0.2.2:3000/api';
+      return 'http://192.168.1.5:3000/api';
     }
     return 'http://127.0.0.1:3000/api';
   }
